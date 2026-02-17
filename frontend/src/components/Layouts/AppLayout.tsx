2572d852d6add9e673a1e7891aefb341a86cc2c0
Gabriel Rodrigues <gabriel.78.rodrigues@gmail.com>
2026-02-16 23:07:44 -0300

    Feat: Add App navbar

--- frontend/src/components/Layouts/AppLayout.tsx
+++ frontend/src/components/Layouts/AppLayout.tsx
@@ -1,14 +1,56 @@
 import { Toaster } from "@/components/ui/sonner";
 import { useAuthStore } from "@/stores/auth";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import logo from "@/assets/Logo.svg";
 
 export function AppLayout() {
-  const { isAuthenticated } = useAuthStore();
+  const { isAuthenticated, user } = useAuthStore();
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const currentTab =
+    location.pathname === "/"
+      ? "dashboard"
+      : location.pathname.split("/").pop() || "home";
 
   if (!isAuthenticated) return <Navigate to={"/login"} replace />;
 
   return (
-    <div className="flex h-screen w-screen overflow-hidden bg-gray-100">
+    <div className="flex h-screen flex-col w-screen overflow-hidden bg-gray-100">
+      <header className="flex w-full min-h-fit shrink-0 bg-background items-center px-12 py-4 justify-between">
+        <figure>
+          <img className="w-[6.25rem] h-6" src={logo} alt="Logo" />
+        </figure>
+
+        <Tabs
+          value={currentTab}
+          onValueChange={(value) =>
+            navigate(value === "dashboard" ? "/" : `/${value}`)
+          }
+        >
+          <TabsList className="bg-transparent">
+            <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+
+            <TabsTrigger value="transactions">Transações</TabsTrigger>
+
+            <TabsTrigger value="categories">Categorias</TabsTrigger>
+          </TabsList>
+        </Tabs>
+
+        <div className="w-[6.25rem] flex justify-end">
+          <button
+            className="flex items-center justify-center shrink-0 size-9 bg-gray-300 text-gray-800 text-sm rounded-full"
+            onClick={() => navigate("/profile")}
+          >
+            <span>
+              {user && user?.name.length > 0 ? user?.name[0].toUpperCase() : ""}
+            </span>
+          </button>
+        </div>
+      </header>
+
       <main className="flex size-full overflow-hidden">
         <Outlet />
       </main>
