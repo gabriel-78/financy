c4e54aba14eeaf85bb6d3ccee89ad927a3910dbd
Gabriel Rodrigues <gabriel.78.rodrigues@gmail.com>
2026-02-17 21:47:37 -0300

    Style: Adjust dashboard page

--- frontend/src/pages/Dashboards.tsx
+++ frontend/src/pages/Dashboards.tsx
@@ -23,7 +23,7 @@ import { useMemo } from "react";
 import { LIST_TRANSACTIONS } from "@/lib/graphql/queries/transaction";
 import type { ListTransactionQueryData } from "./Transactions";
 import { TransactionType, type Transaction } from "@/types/transaction";
-import { endOfMonth, isWithinInterval, startOfMonth } from "date-fns";
+import { endOfMonth, format, isWithinInterval, startOfMonth } from "date-fns";
 
 const moneyFormatter = currencyFormatter();
 
@@ -126,49 +126,53 @@ export function Dashboards() {
 
   return (
     <div className="grid grid-cols-6 p-12 gap-6 w-full overflow-hidden">
-      <Item variant="muted" className="w-full col-span-2">
+      <Item variant="muted" className="w-full col-span-2 gap-4">
         <ItemHeader className="gap-3 items-center justify-start">
           <ItemMedia variant="default">
             <Wallet className="text-purple-base" />
           </ItemMedia>
 
-          <ItemDescription className="uppercase">Saldo total</ItemDescription>
+          <ItemDescription className="uppercase text-xs tracking-[0.0375rem] text-gray-500 font-medium">
+            Saldo total
+          </ItemDescription>
         </ItemHeader>
 
         <ItemContent>
-          <ItemTitle>{`${userBudget >= 0 ? "" : "- "}${moneyFormatter.format(userBudget)}`}</ItemTitle>
+          <ItemTitle className="text-gray-800 font-bold text-[1.75rem] leading-8">{`${userBudget >= 0 ? "" : "- "}${moneyFormatter.format(userBudget)}`}</ItemTitle>
         </ItemContent>
       </Item>
 
-      <Item variant="muted" className="w-full col-span-2">
+      <Item variant="muted" className="w-full col-span-2 gap-4">
         <ItemHeader className="gap-3 items-center justify-start">
           <ItemMedia variant="default">
-            <ArrowUpCircle className="text-brand-base" />
+            <ArrowUpCircle className="text-brand-base size-5" />
           </ItemMedia>
 
-          <ItemDescription className="uppercase">
+          <ItemDescription className="uppercase text-xs tracking-[0.0375rem] text-gray-500 font-medium">
             Receitas do mês
           </ItemDescription>
         </ItemHeader>
 
         <ItemContent>
-          <ItemTitle>{moneyFormatter.format(thisMonthBudget.income)}</ItemTitle>
+          <ItemTitle className="text-gray-800 font-bold text-[1.75rem] leading-8">
+            {moneyFormatter.format(thisMonthBudget.income)}
+          </ItemTitle>
         </ItemContent>
       </Item>
 
-      <Item variant="muted" className="w-full col-span-2">
+      <Item variant="muted" className="w-full col-span-2 gap-4">
         <ItemHeader className="gap-3 items-center justify-start">
           <ItemMedia variant="default">
-            <ArrowDownCircle className="text-red-base" />
+            <ArrowDownCircle className="text-red-base size-5" />
           </ItemMedia>
 
-          <ItemDescription className="uppercase">
+          <ItemDescription className="uppercase text-xs tracking-[0.0375rem] text-gray-500 font-medium">
             Despesas do mês
           </ItemDescription>
         </ItemHeader>
 
         <ItemContent>
-          <ItemTitle>
+          <ItemTitle className="text-gray-800 font-bold text-[1.75rem] leading-8">
             {moneyFormatter.format(thisMonthBudget.expense)}
           </ItemTitle>
         </ItemContent>
@@ -176,8 +180,10 @@ export function Dashboards() {
 
       <div className="flex grow overflow-auto col-span-4 p-px">
         <Card className="max-h-full h-fit overflow-auto w-full">
-          <CardHeader className="flex flex-row items-center gap-4 border-b sticky bg-background border-solid border-b-gray-200 justify-between top-0">
-            <CardTitle>Transações recentes</CardTitle>
+          <CardHeader className="flex flex-row items-center p-5 pr-3 gap-4 border-b sticky bg-background border-solid border-b-gray-200 justify-between top-0">
+            <CardTitle className="uppercase text-xs tracking-[0.0375rem] text-gray-500 font-medium">
+              Transações recentes
+            </CardTitle>
 
             <Button type="button" onClick={() => navigate("/transactions")}>
               Ver todas
@@ -185,23 +191,31 @@ export function Dashboards() {
             </Button>
           </CardHeader>
 
-          <CardContent className="flex h-full flex-col gap-5 pt-6 overflow-auto">
+          <CardContent className="flex h-full flex-col p-0 overflow-auto">
             {transactions.map((transaction) => (
               <div
-                className="grid grid-cols-[1fr_minmax(8rem,10rem)_minmax(5rem,8rem)_] w-full min-h-fit overflow-hidden items-center justify-between gap-3"
+                className="grid grid-cols-[1fr_minmax(8rem,10rem)_minmax(5rem,8rem)_] w-full min-h-fit overflow-hidden items-center justify-between gap-3 px-6 py-5 border-t border-solid border-t-gray-200 first:border-t-transparent"
                 key={transaction.id}
               >
                 <div className="flex grow overflow-hidden items-center justify-start gap-4">
                   <CategoryTag
                     color={transaction.category?.color ?? ""}
-                    className="[&>svg]:size-4 p-3 rounded-[8px] shrink-0"
+                    className="[&>svg]:size-5 size-10 rounded-[8px] shrink-0"
                   >
                     <CategoryIcon
                       mark={transaction.category?.type ?? "GENERAL_EXPENSES"}
                     />
                   </CategoryTag>
 
-                  <span className="truncate">{transaction.description}</span>
+                  <div className="flex flex-col gap-0.5 w-full overflow-hidden">
+                    <strong className="font-medium text-base text-gray-800 truncate">
+                      {transaction.description}
+                    </strong>
+
+                    <small className="font-normal text-sm text-gray-600 truncate">
+                      {format(new Date(transaction.date), "dd/MM/yyyy")}
+                    </small>
+                  </div>
                 </div>
 
                 <div className="flex grow items-center justify-center">
@@ -213,14 +227,14 @@ export function Dashboards() {
                 </div>
 
                 <div className="flex grow items-center justify-end gap-2">
-                  <span className="truncate">
-                    {moneyFormatter.format(transaction.amount)}
+                  <span className="truncate text-sm text-gray-800 font-semibold">
+                    {`${transaction.type === TransactionType.INCOME ? "+" : "-"} ${moneyFormatter.format(transaction.amount)}`}
                   </span>
 
                   {transaction.type === TransactionType.INCOME ? (
-                    <ArrowUpCircle className="text-brand-base" />
+                    <ArrowUpCircle className="text-brand-base size-4" />
                   ) : (
-                    <ArrowDownCircle className="text-red-base" />
+                    <ArrowDownCircle className="text-red-base size-4" />
                   )}
                 </div>
               </div>
@@ -231,8 +245,10 @@ export function Dashboards() {
 
       <div className="flex grow overflow-auto col-span-2 p-px">
         <Card className="max-h-full h-fit overflow-auto w-full">
-          <CardHeader className="flex flex-row items-center gap-4 border-b sticky bg-background border-solid border-b-gray-200 justify-between top-0">
-            <CardTitle>Categorias</CardTitle>
+          <CardHeader className="flex flex-row items-center px-6 py-5 gap-4 border-b sticky bg-background border-solid border-b-gray-200 justify-between top-0">
+            <CardTitle className="uppercase text-xs tracking-[0.0375rem] text-gray-500 font-medium">
+              Categorias
+            </CardTitle>
 
             <Button type="button" onClick={() => navigate("/categories")}>
               Gerenciar
@@ -240,7 +256,7 @@ export function Dashboards() {
             </Button>
           </CardHeader>
 
-          <CardContent className="flex h-full flex-col gap-5 pt-6 overflow-auto">
+          <CardContent className="flex h-full flex-col gap-5 overflow-auto p-6">
             {groupedTransaction.map((category) => (
               <div
                 className="flex w-full min-h-fit overflow-hidden items-center justify-between gap-3"
@@ -251,9 +267,11 @@ export function Dashboards() {
                 </CategoryTag>
 
                 <div className="flex min-w-fit items-center justify-end gap-3 shrink-0">
-                  <span>{`${category.transactionsCount} ${category.transactionsCount !== 1 ? "itens" : "item"}`}</span>
+                  <small className="text-sm font-normal text-gray-600">{`${category.transactionsCount} ${category.transactionsCount !== 1 ? "itens" : "item"}`}</small>
 
-                  <span>{moneyFormatter.format(category.total)}</span>
+                  <strong className="text-sm font-semibold text-gray-800">
+                    {moneyFormatter.format(category.total)}
+                  </strong>
                 </div>
               </div>
             ))}
