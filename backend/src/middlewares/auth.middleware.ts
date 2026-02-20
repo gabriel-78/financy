4df357621565d1fc3c054d1520bfbd3bda395e43
Gabriel Rodrigues <gabriel.78.rodrigues@gmail.com>
2026-02-19 21:00:23 -0300

    Fix: Auth incoerent code when not authenticated user

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,9 +1,14 @@
 import { GraphqlContext } from '@/graphql/contexts';
+import { GraphQLError } from 'graphql/error';
 import { MiddlewareFn } from 'type-graphql';
 
 export const IsAuth: MiddlewareFn<GraphqlContext> = async ({ context }, next) => {
   if (!context.user) {
-    throw new Error('User not authenticated');
+    throw new GraphQLError('User not authenticated', {
+      extensions: {
+        code: 'UNAUTHENTICATED',
+      },
+    });
   }
 
   return next();
